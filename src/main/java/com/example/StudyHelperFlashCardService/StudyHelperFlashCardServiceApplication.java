54bc9248d49a21abe33d0dff4bc45a03977fbf02
Jason Benckert <jason.benckert@cognizant.com>
2021-07-01 18:36:54 -0400

    project initialization

---
+++ src/main/java/com/example/StudyHelperFlashCardService/StudyHelperFlashCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudyHelperFlashCardService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperFlashCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHelperFlashCardServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StudyHelperFlashCardService/StudyHelperFlashCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyHelperFlashCardService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHelperFlashCardServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
